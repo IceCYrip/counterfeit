0f4174fb8d9f2a7d5914b129d6d6fd56421a9f1c
IceCYrip <karansable16@gmail.com>
2023-10-26 01:44:58 +0530

    SignUp done

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import './styles/SignUp.css'
 import { useNavigate } from 'react-router-dom'
 
+import axios from 'axios'
+
+import backendURL from './url'
+
 const SignUp = () => {
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [userType, setUserType] = useState('manufacturer')
+
   const routeTo = useNavigate()
+
+  const signUp = () => {
+    const bodyForAPI = {
+      fullName,
+      email,
+      password,
+      userType,
+    }
+    console.log('Data: ', bodyForAPI)
+
+    axios
+      .post(`${backendURL}/auth/createuser`, bodyForAPI)
+      .then((res) => {
+        window.alert('Account Created Successfully. Please login now')
+        routeTo('/manufacturer')
+      })
+      .catch((error) => window.alert('Something went wrong'))
+  }
 
   return (
     <div className='signUpWrapper'>
@@ -22,11 +49,17 @@ const SignUp = () => {
                 type='text'
                 placeholder='Enter your name'
                 name='nameOfUser'
+                onChange={(e) => setFullName(e.target.value)}
               />
             </div>
             <div className='signUpFieldsContainer'>
               <label htmlFor='email'>Email</label>
-              <input type='text' placeholder='Enter your email' name='email' />
+              <input
+                type='text'
+                placeholder='Enter your email'
+                name='email'
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className='signUpFieldsContainer'>
               <label htmlFor='password'>Password</label>
@@ -34,11 +67,12 @@ const SignUp = () => {
                 type='text'
                 placeholder='Create a password'
                 name='password'
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
             <div className='signUpBttnGrp'>
-              <button className='signInButton' onClick={() => {}}>
+              <button className='signInButton' onClick={() => signUp()}>
                 Sign Up
               </button>
               <label className='signInText'>
