676212beff7fc161f5a525b1582048dd1a7e86fe
IceCYrip <karansable16@gmail.com>
2024-01-13 13:20:42 +0530

    bhai karan kiti maati khashil.. (URL corrected again)

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
 // const backendServerURL = 'http://localhost:5000/api'
-const backendServerURL = 'https://counterfeit-backend.vercel.app'
+const backendServerURL = 'https://counterfeit-backend.vercel.app/api'
 
 export default backendServerURL
