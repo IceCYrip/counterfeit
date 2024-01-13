36c7749ef1e373e982ab8d4201d90d9f398e50c0
IceCYrip <karansable16@gmail.com>
2024-01-13 13:18:45 +0530

    url mistake corrected

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
 // const backendServerURL = 'http://localhost:5000/api'
-const backendServerURL = 'https://https://counterfeit-backend.vercel.app'
+const backendServerURL = 'https://counterfeit-backend.vercel.app'
 
 export default backendServerURL
