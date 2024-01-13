8acadec71c2381ce29fd9254a0c016ae817f3884
IceCYrip <karansable16@gmail.com>
2024-01-13 13:15:42 +0530

    url changed to production url

--- src/url.js
+++ src/url.js
@@ -1,3 +1,4 @@
-const backendServerURL = 'http://localhost:5000/api'
+// const backendServerURL = 'http://localhost:5000/api'
+const backendServerURL = 'https://https://counterfeit-backend.vercel.app'
 
 export default backendServerURL
