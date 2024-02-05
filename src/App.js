41a0afac04afb1ba851e2a517aac5723aa6e46a9
IceCYrip <karansable16@gmail.com>
2024-02-05 14:15:36 +0530

    Verify button on dashboard redirects to qr scanner

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import React, { useEffect } from 'react'
-import './styles/App.css'
-import Navbar from './components/Navbar'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import "./styles/App.css";
+import Navbar from "./components/Navbar";
+import { Link } from "react-router-dom";
 
 function App() {
-  const userDataString = localStorage.getItem('userData') ?? ''
-  const userData = userDataString ? JSON.parse(userDataString ?? '') : {}
+  const userDataString = localStorage.getItem("userData") ?? "";
+  const userData = userDataString ? JSON.parse(userDataString ?? "") : {};
 
   useEffect(() => {
-    localStorage.setItem('activeMenu', 'home')
-  }, [])
+    localStorage.setItem("activeMenu", "home");
+  }, []);
 
   return (
-    <div className='homeWrapper'>
+    <div className="homeWrapper">
       <Navbar />
 
-      <div className='homeBody'>
-        <div className='bodyLeft'>
+      <div className="homeBody">
+        <div className="bodyLeft">
           <h1>Your Trust, Our Technology</h1>
-          <div className='bttnGrp'>
-            <span className='dashboardButton'>
+          <div className="bttnGrp">
+            <span className="dashboardButton">
               {/* <Link to="/manufacturer">Add Product</Link> */}
-              <Link to={userData?.id ? '/dashboard' : '/manufacturer'}>
+              <Link to={userData?.id ? "/dashboard" : "/manufacturer"}>
                 Manufacturer
               </Link>
             </span>
-            <span className='dashboardButton'>
-              <Link to='/verify'>Verify Product</Link>
+            <span className="dashboardButton">
+              <Link to="/buyer">Verify Product</Link>
             </span>
           </div>
         </div>
         <br />
         <img
-          className='dashboardImg'
-          src='/dashboardImage.png'
-          alt='Dashboard'
+          className="dashboardImg"
+          src="/dashboardImage.png"
+          alt="Dashboard"
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
